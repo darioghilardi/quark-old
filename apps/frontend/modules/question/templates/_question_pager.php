2dc4352e48eba0a06050fd1337b0c43937a3c822
Domenico Monaco <domenico.monaco@kiuz.it>
2011-01-31 00:49:18 +0100

    cambi vari nel markup per usare la graglia

--- apps/frontend/modules/question/templates/_question_pager.php
+++ apps/frontend/modules/question/templates/_question_pager.php
@@ -1,32 +1,30 @@
-<?php if(($pager->haveToPaginate()) && ($pager->getPage() > 1)): ?>
-	<div>
-		<?php echo link_to('<<', 'question/index?page='.$pager->getFirstPage().'&order='.$order) ?>
-	</div>
-
-	<div>
-		<?php echo link_to('<', 'question/index?page='.$pager->getPreviousPage().'&order='.$order) ?>
-	</div>
-<?php endif ?>
-
-<div>
-	<?php $links = $pager->getLinks(); foreach ($links as $page): ?>
-		<div>
-			<?php echo ($page == $pager->getPage()) ? $page : link_to($page, 'question/index?page='.$page.'&order='.$order) ?>
-		</div>
-	<?php endforeach ?>
-</div>
-
-<?php if(($pager->haveToPaginate()) && ($pager->getPage() < $pager->getNextPage())): ?>
-	<div>
-		<?php echo link_to('>', 'question/index?page='.$pager->getNextPage().'&order='.$order) ?>
-	</div>
-
-	<div>
-		<?php echo link_to('>>', 'question/index?page='.$pager->getLastPage().'&order='.$order) ?>
-	</div>
-<?php endif ?>
-
-<div class="pagination_desc">
-  <strong><?php echo count($pager) ?></strong> questions
-  - page <strong><?php echo $pager->getPage() ?>/<?php echo $pager->getLastPage() ?></strong>
+<div id="pagination">
+	<ul id="pager" class="nonelist">
+		<?php if(($pager->haveToPaginate()) && ($pager->getPage() > 1)): ?>
+			<li class="boxleft">
+			  <?php echo link_to('<<', 'question/index?page='.$pager->getFirstPage().'&order='.$order) ?>
+			</li>
+			<li class="boxleft">
+			  <?php echo link_to('<', 'question/index?page='.$pager->getPreviousPage().'&order='.$order) ?>
+			</li>
+		<?php endif ?>
+	
+		<?php $links = $pager->getLinks(); foreach ($links as $page): ?>
+			<li class="boxleft">
+				<?php echo ($page == $pager->getPage()) ? $page : link_to($page, 'question/index?page='.$page.'&order='.$order) ?>
+			</li>
+		<?php endforeach ?>	
+		<?php if(($pager->haveToPaginate()) && ($pager->getPage() < $pager->getNextPage())): ?>
+		  <li class="boxleft">
+		    <?php echo link_to('>', 'question/index?page='.$pager->getNextPage().'&order='.$order) ?>
+		  </li>
+		  <li class="boxleft">
+		    <?php echo link_to('>>', 'question/index?page='.$pager->getLastPage().'&order='.$order) ?>
+		  </li>
+		<?php endif ?>
+	</ul>
+	
+	<p id="pager_desc">
+	  <strong><?php echo count($pager) ?></strong> questions - page <strong><?php echo $pager->getPage() ?>/<?php echo $pager->getLastPage() ?></strong>
+	</p>
 </div>
