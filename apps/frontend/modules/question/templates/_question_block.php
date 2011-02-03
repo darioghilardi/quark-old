3b554e291475f1056bd9bf638bc7f6f58d0a1bce
Domenico Monaco <domenico.monaco@kiuz.it>
2011-02-03 18:51:21 +0100

    sidebar style, block partial adn style

---
+++ apps/frontend/modules/question/templates/_question_block.php
@@ -0,0 +1,8 @@
+<div class="block">
+  <?php if(!empty($title)):?>
+  <h4 class="block-title"><?php print $title; ?></h4>
+  <?php endif; ?>
+  <?php if(!empty($content)):?>
+    <div><?php print $content ?></div>
+  <?php endif; ?>
+</div>
--- apps/frontend/modules/question/templates/indexSuccess.php
+++ apps/frontend/modules/question/templates/indexSuccess.php
@@ -14,19 +14,13 @@
 	</div>
 	
 	<div id="sidebar" class="col-6">
-    <div class="block">
-      <h2>Primo</h2>
-      <p>Bloa bla blaoa </p>
-    </div>
-    
-    <div class="block">
-          <h2>Secondo</h2>
-      <p>Bloa bla blaoa </p>
-    </div>
-    
-    <div class="block">
-          <h2>Terzo</h2>
-      <p>Bloa bla blaoa </p>
-    </div>
-	</div>
+	
+  <?php include_partial('question/question_block', array('title' => 'Introduzione a Quark', 'content' => 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for  will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like')) ?>
+  
+  <?php include_partial('question/question_block', array('title' => 'Come partecipare', 'content' => 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using  making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for  will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)')) ?>
+  
+  <?php include_partial('question/question_block', array('title' => 'Collabora', 'content' => 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)
+  .')) ?>
+  
+  </div>
 </div>
