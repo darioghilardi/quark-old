2cb2b8c2befc7aa1bc11d98ac4416bec2fe43336
Domenico Monaco <domenico.monaco@kiuz.it>
2011-02-08 19:13:22 +0100

    login page layout

--- apps/frontend/modules/sfGuardAuth/templates/_signin_form.php
+++ apps/frontend/modules/sfGuardAuth/templates/_signin_form.php
@@ -1,10 +1,27 @@
 <?php use_helper('I18N') ?>
 
-<form action="<?php echo url_for('@sf_guard_signin') ?>" method="post">
+<form id="signin-form" action="<?php echo url_for('@sf_guard_signin') ?>" method="post">
+  <?php echo $form->renderGlobalErrors() ?>
+  
+  <div id="username-signin" class="item">
+	  <?php echo $form['username']->renderError() ?>
+	  <?php echo $form['username']->renderLabel() ?>
+	  <?php echo $form['username'] ?>
+  </div>
+  
+  <div id="password-signin" class="item">
+	  <?php echo $form['password']->renderError() ?>
+	  <?php echo $form['password']->renderLabel() ?>
+	  <?php echo $form['password'] ?>
+  </div>
+  
+	<?php echo $form['_csrf_token'] ?>
+	
+  <span id ="submit-question" class="boxright">
+    <input type="submit" value="<?php echo __('Signin', null, 'sf_guard') ?>" />
+  </span>
 
-      <?php echo $form ?>
-      <input type="submit" value="<?php echo __('Signin', null, 'sf_guard') ?>" />
-          
+
           <?php $routes = $sf_context->getRouting()->getRoutes() ?>
           <?php if (isset($routes['sf_guard_forgot_password'])): ?>
             <a href="<?php echo url_for('@sf_guard_forgot_password') ?>"><?php echo __('Forgot your password?', null, 'sf_guard') ?></a>
@@ -13,6 +30,5 @@
           <?php if (isset($routes['sf_guard_register'])): ?>
             &nbsp; <a href="<?php echo url_for('@sf_guard_register') ?>"><?php echo __('Want to register?', null, 'sf_guard') ?></a>
           <?php endif; ?>
-
 
 </form>
--- apps/frontend/modules/sfGuardAuth/templates/signinSuccess.php
+++ apps/frontend/modules/sfGuardAuth/templates/signinSuccess.php
@@ -1,18 +1,13 @@
 <?php use_helper('I18N') ?>
 <div id="signinSuccess">
-  <div id="signing-form" class="col-16">
-    <div>
-      <h1><?php echo __('Signin', null, 'sf_guard') ?></h1>
+  <div id="signin-form-content" class="col-16">
+    <h1 id="page-title"><?php echo __('Signin', null, 'sf_guard') ?></h1>
       <?php echo get_partial('sfGuardAuth/signin_form', array('form' => $form)) ?>
-    </div>
-    
   </div>
   
-  <div id="signing-sidebar" class="col-8">
-    <div>
-	    <h4>How to register</h4>
-	    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using </p>
-    </div>
+  <div id="sidebar" class="col-8">
+  <?php include_partial('question/question_block', array('title' => 'Collabora', 'content' => 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).')) ?>
+  
   </div>
   
 </div>
