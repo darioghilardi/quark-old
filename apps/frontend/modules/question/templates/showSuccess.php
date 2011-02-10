c03427a1caa3818865b5f3faf0bee9cb662028f2
Domenico Monaco <kiuz.4ever@gmail.com>
2011-02-10 19:35:37 +0100

    styling answers list into Question single Page - vote and item question

--- apps/frontend/modules/question/templates/showSuccess.php
+++ apps/frontend/modules/question/templates/showSuccess.php
@@ -38,11 +38,39 @@
       
     </div>
 
-</div>
+
+  <div id="answers-list" class="col-18">
+    <h3 id="sub-page-title">Answer List</h3>
+    <?php foreach ($question->getAskQuestion() as $answer): ?>
+      <div class="answer item clearfix">
+        <div class="col-2">
+         <div class="vote">
+           <?php include_partial('question/question_answers_votes_button', array('answer' => $answer)) ?>
+           </div>
+        </div>
+    <div class="col-16">
+    <?php echo $answer->getBody() ?>
+    
+    
+          posted by <?php echo $answer->getUser() ?>
+      on <?php echo $answer->getDateTimeObject('created_at')->format('d/m/Y') ?>
+    </div>
 
 
+    </div>
+  <?php endforeach; ?>
+  </div>
 
+	<div id="answer-form">
+	  <h3>Post a new answer</h3>
+	  <?php if ($sf_user->isAuthenticated()): ?>
+	    <?php include_partial('answer/answer_form', array('form' => $form)) ?>
+	  <?php else: ?>
+	    You need to be logged in to post a new answer.
+	  <?php endif; ?>
+	</div>
 
+  </div>
 
   <div id="sidebar" class="col-6">
 	  <?php include_partial('question/question_block', array('title' => 'Introduzione a Quark', 'content' => 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for  will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like')) ?>
@@ -50,39 +78,4 @@
 	  <?php include_partial('question/question_block', array('title' => 'Collabora', 'content' => 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).')) ?>
   </div>
 
-
-
-
-
-<div id="answers">
-  <h3>Answer List</h3>  
-  <?php foreach ($question->getAskQuestion() as $answer): ?>
-    <div class="answer">
-    Votes: <?php echo $answer->getVotes() ?>
-    <div>
-        <?php echo $answer->getBody() ?>
-
-      </div>
-      posted by <?php echo $answer->getUser() ?>
-      on <?php echo $answer->getDateTimeObject('created_at')->format('d/m/Y') ?>
-    </div>
-  <?php endforeach; ?>
-</div>
-
-<div id="answer-form">
-  <h3>Post a new answer</h3>
-  <?php if ($sf_user->isAuthenticated()): ?>
-    <?php include_partial('answer/answer_form', array('form' => $form)) ?>
-  <?php else: ?>
-    You need to be logged in to post a new answer.
-  <?php endif; ?>
-</div>
-
-
-
-<?php
-/**
- * end of Main
-**/
-?>
 </div>
