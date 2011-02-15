9f16ddcb4c6866e598188bfd35717260b00be84c
Domenico Monaco <kiuz.4ever@gmail.com>
2011-02-15 12:25:33 +0100

    style css and change markup for Answer list into PageQuestion

--- apps/frontend/modules/question/templates/showSuccess.php
+++ apps/frontend/modules/question/templates/showSuccess.php
@@ -64,7 +64,7 @@
 			  <div class="options clearfix clear">
 	        <ul class="nonelist nonespace">
 	          <li class="boxleft"><a href="##">Link</a></li>
-	          <li class="boxleft"><a href="<?php echo url_for('question/edit?id=' . $question->getId()) ?>">Edit</a></li>
+	          <li class="boxleft"><a href="<?php echo url_for('answer/edit?id=' . $question->getId()) ?>">Edit</a></li>
 	          <li class="boxleft"><a href="##">Flag</a></li>
 	        </ul>
 	      </div>
