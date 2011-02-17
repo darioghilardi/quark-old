c8ad5ab94ca3f3c44a9ae32f5ca62740c8911201
Domenico Monaco <kiuz.4ever@gmail.com>
2011-02-17 18:08:46 +0100

    functional test

--- test/functional/frontend/questionActionsTest.php
+++ test/functional/frontend/questionActionsTest.php
@@ -5,11 +5,39 @@ include(dirname(__FILE__).'/../../bootstrap/functional.php');
 $browser = new QuarkTestFunctional(new sfBrowser());
 $browser->loadData();
 
-$max = 20;
 
+$max = 1;
+$browser->info('0 - The homepage')->
+  get('/')->
+  info(sprintf('There are max %s title h1 into page', $max))->
+  with('response')->checkElement('h1', $max);
+  
+
+$max = 20;
 $browser->info('1 - The homepage')->
   get('/')->
   info(sprintf('  1.1 - Only %s questions are listed into the homepage', $max))->
   with('response')->
     checkElement('#question-list-content > div.item', $max)
 ;
+
+$max = 20;
+$browser->info('2 - The homepage')->
+  get('/')->
+  info(sprintf('Only %s questions are listed into the homepage with titles', $max))->
+  with('response')->
+    checkElement('#question-list-content > div.item .title', $max);
+
+$browser->info('3 - The homepage')->
+  get('/')->
+  info(sprintf('Click links %s questions are listed into the homepage with', $max))->
+  with('response')->
+    click('#question-list-content > div.item a', array(), array('position' => 1))
+    ->with('response')->checkElement('#showSuccess', true)
+    ->with('response')->checkElement('#indexSuccess', false)
+    ->with('response')->checkElement('h1', 1)
+    ->back()
+    ->with('response')->checkElement('#showSuccess', false)
+    ->with('response')->checkElement('#indexSuccess', true)
+    ;
+    
