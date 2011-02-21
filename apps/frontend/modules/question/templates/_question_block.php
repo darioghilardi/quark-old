6549561346b7c1375df5c21ab6ac12c0e48f94b0
Domenico Monaco <kiuz.4ever@gmail.com>
2011-02-21 16:46:28 +0100

    adding custom markup into indexSuccess for newm block style

--- apps/frontend/modules/question/templates/_question_block.php
+++ apps/frontend/modules/question/templates/_question_block.php
@@ -1,8 +1,8 @@
-<div class="block">
+<div class="block <?php (isset($type))? print $type : print 'standard';?>">
   <?php if(!empty($title)):?>
   <h4 class="block-title"><?php print $title; ?></h4>
   <?php endif; ?>
   <?php if(!empty($content)):?>
-    <div><?php print $content ?></div>
+    <div><?php echo $content ?></div>
   <?php endif; ?>
 </div>
--- apps/frontend/modules/question/templates/indexSuccess.php
+++ apps/frontend/modules/question/templates/indexSuccess.php
@@ -14,7 +14,49 @@
 	</div>
 	
 	<div id="sidebar" class="col-6">
-	
+
+  <?php //Custom Blocks ?>
+    <div class="block welcome">
+      <h4 class="block-title">Welcome!</h4>
+      <div>
+      <p>This is a collaboratively edited question and answer site for power users of web applications. It's 100% free, no registration required.</p>
+      </div>
+    </div>
+  
+	  <div class="block statistics">
+		  <div>
+        <div class="questions-count">1246<span>questions</span></div>
+        <div class="answers-count">2503<span>answers</span></div>
+		  </div>
+	  </div>
+
+    <div class="block populartags">
+      <h4 class="block-title">Popular Tags</h4>
+      <div>
+      <ul class="nonelist nonespace">
+        <li><a href="">Web</a><?php print 'x' .rand(5,99);?></li>
+        <li><a href="">Developing</a><?php print 'x' .rand(5,99);?></li>
+        <li><a href="">Apache</a><?php print 'x' .rand(5,99);?></li>
+        <li><a href="">Google</a><?php print 'x' .rand(5,99);?></li>
+        <li><a href="">Driven</a><?php print 'x' .rand(5,99);?></li>s
+        <li><a href="">Design</a><?php print 'x' .rand(5,99);?></li>
+        <li><a href="">Android</a><?php print 'x' .rand(5,99);?></li>
+        <li><a href="">Image</a><?php print 'x' .rand(5,99);?></li>
+        <li><a href="">Css3</a><?php print 'x' .rand(5,99);?></li>
+        <li><a href="">XHTML</a><?php print 'x' .rand(5,99);?></li>
+        <li><a href="">Django</a><?php print 'x' .rand(5,99);?></li>
+        <li><a href="">Drupal</a><?php print 'x' .rand(5,99);?></li>
+        <li><a href="">Python</a><?php print 'x' .rand(5,99);?></li>
+        <li><a href="">Linux</a><?php print 'x' .rand(5,99);?></li>
+        <li><a href="">Programming</a><?php print 'x' .rand(5,99);?></li>
+      </ul>
+      </div>
+    </div>
+
+
+    
+	  <?php //END ?>
+  
   <?php include_partial('question/question_block', array('title' => 'Introduzione a Quark', 'content' => 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for  will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like')) ?>
   
   <?php include_partial('question/question_block', array('title' => 'Come partecipare', 'content' => 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using  making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for  will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)')) ?>
