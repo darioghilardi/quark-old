8151cd56db39cbff3395565fb1d14265685dd97c
Domenico Monaco <kiuz.4ever@gmail.com>
2011-02-22 16:19:03 +0100

    fied new PATH for USer Profile Page into Primary Menu Information after Login

--- apps/frontend/modules/question/templates/_question_primary_menu.php
+++ apps/frontend/modules/question/templates/_question_primary_menu.php
@@ -9,7 +9,8 @@
 <ul id="primary-menu" class="nonelist nonespace boxright txtright">
         <li class="boxright login-item">
             <?php if((empty($notloginlink))): ?>
-	            <?php if ($sf_user->isAuthenticated()): ?> Welcome <?php echo $sf_user ?> <a href="<?php print url_for('@sf_guard_user') . '/'. $sf_user->getGuardUser()->getId() . '/edit';?>">Edit</a> - <?php echo link_to('Logout', 'sf_guard_signout') ?>
+	            <?php if ($sf_user->isAuthenticated()): ?> Welcome <?php echo link_to($sf_user, 'user/show?username=' . $sf_user->getGuardUser()->getUsername()) ?> - <?php echo link_to('Logout', 'sf_guard_signout') ?>
+	            
 	            <?php else: ?>
 	              <a href="<?php echo url_for('sf_guard_signin') ?>">Login</a>
 	            <?php endif;?>
@@ -20,4 +21,3 @@
         <li class="boxright menu-item"><a href="">About</a></li>
         <li class="boxright menu-item"><a href="">Blabla</a></li>
       </ul>
-      
