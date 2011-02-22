8f601ebc6eaacca1424b89d5b54fb042a361fbf2
Domenico Monaco <kiuz.4ever@gmail.com>
2011-02-22 11:58:04 +0100

    user showSuccess page

--- apps/frontend/modules/user/actions/actions.class.php
+++ apps/frontend/modules/user/actions/actions.class.php
@@ -23,6 +23,8 @@ class userActions extends sfActions
   public function executeShow(sfWebRequest $request)
   {
   	
+    $question = Doctrine::getTable('sfGuardUser')->find($request->getParameter('id'));
+    
   }
   
 }
