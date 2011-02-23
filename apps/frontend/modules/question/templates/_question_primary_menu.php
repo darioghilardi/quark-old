67df421b88526142f71a42dda8a7cf147e42dab9
Domenico Monaco <kiuz.4ever@gmail.com>
2011-02-23 16:04:09 +0100

    added avatar into PrimaryMenu when oser logged in

--- apps/frontend/modules/question/templates/_question_primary_menu.php
+++ apps/frontend/modules/question/templates/_question_primary_menu.php
@@ -1,23 +1,15 @@
-<?php 
-/*
- * <?php echo link_to('Users', '@sf_guard_user') ?>/
- * <?php echo $sf_user->getGuardUser()->getId(); ?>
- * 
-**/
-?>
+<?php use_helper('Gravatar') ?>
 
 <ul id="primary-menu" class="nonelist nonespace boxright txtright">
-        <li class="boxright login-item">
-            <?php if((empty($notloginlink))): ?>
-	            <?php if ($sf_user->isAuthenticated()): ?> Welcome <?php echo link_to($sf_user->getGuardUser()->getUsername(), 'user/show?username=' . $sf_user->getGuardUser()->getUsername()) ?> - <?php echo link_to('Logout', 'sf_guard_signout') ?>
-	            
-	            <?php else: ?>
-	              <a href="<?php echo url_for('sf_guard_signin') ?>">Login</a>
-	            <?php endif;?>
-            <?php endif;?>
-        </li>
-
-        <li class="boxright menu-item"><a href="">Home</a></li>
-        <li class="boxright menu-item"><a href="">About</a></li>
-        <li class="boxright menu-item"><a href="">Blabla</a></li>
-      </ul>
+  <li class="boxright login-item">
+    <?php if((empty($notloginlink))): ?>
+    <?php if ($sf_user->isAuthenticated()): ?> Welcome <?php echo gravatar($sf_user->getGuardUser()->getEmail_address(), 13);?><?php echo link_to($sf_user->getGuardUser()->getUsername(), 'user/show?username=' . $sf_user->getGuardUser()->getUsername()) ?> - <?php echo link_to('Logout', 'sf_guard_signout') ?>
+    <?php else: ?>
+      <a href="<?php echo url_for('sf_guard_signin') ?>">Login</a>
+    <?php endif;?>
+    <?php endif;?>
+  </li>
+  <li class="boxright menu-item"><a href="">Home</a></li>
+  <li class="boxright menu-item"><a href="">About</a></li>
+  <li class="boxright menu-item"><a href="">Blabla</a></li>
+</ul>
