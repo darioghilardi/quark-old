aa7df6c7036963e233a4c7d8a8ac021988113eca
Domenico Monaco <kiuz.4ever@gmail.com>
2011-02-23 16:07:22 +0100

    start adding avatar into question list

--- apps/frontend/modules/question/templates/_question_submitted.php
+++ apps/frontend/modules/question/templates/_question_submitted.php
@@ -1,1 +1,4 @@
-<span class="time"><?php print rand(0,20); ?><?php (rand(0,1)==1) ? print 'h' : print 'min';?> ago</span> by <span class="user"><a href=""><?php (rand(0,1)==1) ? print 'kiuz' : print 'Ingo';?></a></span> <span class="points"><?php print rand(299,2000); ?></span>
+<?php use_helper('Gravatar') ?>
+
+<span class="time"><?php print rand(0,20); ?>
+<?php (rand(0,1)==1) ? print 'h' : print 'min';?> ago</span> by <span class="user"><a href=""><?php (rand(0,1)==1) ? print 'kiuz' : print 'Ingo';?></a></span> <span class="points"><?php print rand(299,2000); ?></span>
