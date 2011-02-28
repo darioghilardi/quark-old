bf1a3696b3c5843c5b38522ef3cfed3c5d6bd25c
Domenico Monaco <kiuz.4ever@gmail.com>
2011-02-28 17:17:41 +0100

    added third menu with new links for footer menu

--- apps/frontend/modules/question/templates/_question_primary_menu.php
+++ apps/frontend/modules/question/templates/_question_primary_menu.php
@@ -1,16 +1,17 @@
 <?php use_helper('Gravatar') ?>
 
 <ul id="primary-menu" class="nonelist nonespace boxright txtright">
-  <li class="boxright login-item">
-    <?php if((empty($notloginlink))): ?>
-    <?php if ($sf_user->isAuthenticated()): ?> Welcome <?php echo gravatar($sf_user->getGuardUser()->getEmail_address(), 13);?><?php echo link_to($sf_user->getGuardUser()->getUsername(), 'user/show?username=' . $sf_user->getGuardUser()->getUsername()) ?> - <?php echo link_to('Logout', 'sf_guard_signout') ?>
-    <?php else: ?>
-      <a href="<?php echo url_for('sf_guard_signin') ?>">Login</a>
-      or 
-      <a href="<?php echo url_for('sf_guard_signin') ?>">Register</a>
-    <?php endif;?>
-    <?php endif;?>
-  </li>
+  <?php if((empty($notloginlink))): ?>
+	  <li class="boxright login-item">
+	    <?php if ($sf_user->isAuthenticated()): ?> Welcome <?php echo gravatar($sf_user->getGuardUser()->getEmail_address(), 13);?><?php echo link_to($sf_user->getGuardUser()->getUsername(), 'user/show?username=' . $sf_user->getGuardUser()->getUsername()) ?> - <?php echo link_to('Logout', 'sf_guard_signout') ?>
+	    <?php else: ?>
+	      <a href="<?php echo url_for('sf_guard_signin') ?>">Login</a>
+	      or 
+	      <a href="<?php echo url_for('sf_guard_signin') ?>">Register</a>
+	    <?php endif;?>
+	  </li>
+  <?php endif;?>
+  
   <li class="boxright menu-item"><?php echo link_to('Faq', '/faq') ?></li>
   <li class="boxright menu-item"><?php echo link_to('About', '/about') ?></li>
   <li class="boxright menu-item"><a href="/">Home</a></li>
---
+++ apps/frontend/modules/question/templates/_question_third_menu.php
@@ -0,0 +1,4 @@
+<ul id="third-menu" class="nonelist nonespace boxright txtright">
+  <li class="boxright menu-item"><?php echo link_to('Terms of Service', '/tos') ?></li>
+  <li class="boxright menu-item"><?php echo link_to('Privacy Policy', '/privacy') ?></li>
+</ul> 
