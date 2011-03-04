65d23ded9e681eb1b5ceb3301984075d5a21cf7b
Domenico Monaco <kiuz.4ever@gmail.com>
2011-03-04 11:20:41 +0100

    refs #7 styled error same as global message

--- apps/frontend/modules/sfApply/templates/applyAfter.php
+++ apps/frontend/modules/sfApply/templates/applyAfter.php
@@ -3,10 +3,11 @@
     <div id="global-content">
       <h1 id="page-title">Thanks for registration</h1>
       
-        <p>
+        <p class="notice">
         <?php echo(__('Thank you for applying for an account. You will receive a verification
         email shortly. If you do not see that email, please be sure to check your "spam" or "bulk" folder.', array(), 'sfForkedApply')) ?>
         </p>
+        
         <?php include_partial('sfApply/continue') ?>
 
     </div>
