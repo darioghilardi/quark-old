c912dd80277bc1156b020523f566f3dc634ac309
Domenico Monaco <kiuz.4ever@gmail.com>
2011-03-04 11:01:20 +0100

    refs #7 added template file for Message AfterRegistration page

---
+++ apps/frontend/modules/sfApply/templates/applyAfter.php
@@ -0,0 +1,14 @@
+<div id="applyAfter">
+  <div id="apply-after-content" class="col-18">
+    <div id="global-content">
+      <h1 id="page-title">Thanks for registration</h1>
+      
+        <p>
+        <?php echo(__('Thank you for applying for an account. You will receive a verification
+        email shortly. If you do not see that email, please be sure to check your "spam" or "bulk" folder.', array(), 'sfForkedApply')) ?>
+        </p>
+        <?php include_partial('sfApply/continue') ?>
+
+    </div>
+  </div>
+</div>
