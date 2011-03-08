2af73f899cc6dcafdb244c19cd10bb16684d8cef
Domenico Monaco <kiuz.4ever@gmail.com>
2011-03-08 12:47:50 +0100

    test #27 styled Successfull message of requesst password for not logged

--- apps/frontend/modules/sfApply/templates/resetRequestAfter.php
+++ apps/frontend/modules/sfApply/templates/resetRequestAfter.php
@@ -1,16 +1,24 @@
 <?php use_helper('I18N') ?>
 <?php slot('sf_apply_login') ?>
 <?php end_slot() ?>
-<div class="success">
-<?php echo __('<p>
-For security reasons, a confirmation message has been sent to
-the email address associated with this account. Please check your
-email for that message. You will need to click on a link provided
-in that email in order to change your password. If you do not see
-the message, be sure to check your "spam" and "bulk" email folders.
-</p>
-<p>
-We apologize for the inconvenience.
-</p>', array(), 'sfForkedApply') ?>
-<?php include_partial('sfApply/continue') ?>
+
+<div id="applyResetRequestAfter">
+  <div id="apply-content" class="col-18">
+    <div id="global-content">
+      <h1 id="page-title">New password request Succesfull!</h1>
+      
+      <p class="success">
+				<?php echo __('
+				For security reasons, a confirmation message has been sent to
+				the email address associated with this account. Please check your
+				email for that message. You will need to click on a link provided
+				in that email in order to change your password. If you do not see
+				the message, be sure to check your "spam" and "bulk" email folders.
+				<br />
+				We apologize for the inconvenience.', array(), 'sfForkedApply') ?>
+				</p>
+				
+				<?php include_partial('sfApply/continue') ?>
+    </div>
+  </div>
 </div>
