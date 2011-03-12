3597c3f181b78ebac62ae5a21a5fdda5944a3a98
Domenico Monaco <kiuz.4ever@gmail.com>
2011-03-12 17:26:20 +0100

     new tags styl

--- apps/frontend/modules/question/templates/_blockrecenttags.php
+++ apps/frontend/modules/question/templates/_blockrecenttags.php
@@ -3,9 +3,21 @@
   <div>
   <ul class="nonelist nonespace">
     <?php foreach ($tags as $tag): ?>
-      <li><a href="<?php echo url_for('@question_list_tags?tags='.$tag['name']); ?>"><?php echo $tag['name']; ?></a>
-        <a href="<?php echo Tagged::addTagsToUrl($sf_params, $tag['name']);?>">+</a>
+      <li><a class="withadd" title="Search for this tags" href="<?php echo url_for('@question_list_tags?tags='.$tag['name']); ?>"><?php echo $tag['name']; ?></a><a class="addtags" title="Add tags into search" href="<?php echo Tagged::addTagsToUrl($sf_params, $tag['name']);?>">+</a>
         X <?php echo $tag['COUNT']; ?></li>
+    <?php endforeach; ?>
+  </ul>
+  </div>
+</div>
+
+<div class="block populartags">
+  <h4 class="block-title">Recent Tags</h4>
+  <div>
+  <ul class="nonelist nonespace">
+    <?php foreach ($tags as $tag): ?>
+      <li>
+      <span class="etichetta"><a class="withadd" title="Search for this" href="<?php echo url_for('@question_list_tags?tags='.$tag['name']); ?>"><?php echo $tag['name']; ?></a><a class="addtags" title="Add tags into search" href="<?php echo Tagged::addTagsToUrl($sf_params, $tag['name']);?>">+</a>
+        </span>X <?php echo $tag['COUNT']; ?></li>
     <?php endforeach; ?>
   </ul>
   </div>
