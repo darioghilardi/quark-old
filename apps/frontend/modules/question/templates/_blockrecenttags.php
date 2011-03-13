68a300cd6951581581d261f4e2d868d665c7963c
Dario Ghilardi <darioghilardi@gmail.com>
2011-03-13 17:06:18 +0100

    Fixed css bug.

--- apps/frontend/modules/question/templates/_blockrecenttags.php
+++ apps/frontend/modules/question/templates/_blockrecenttags.php
@@ -4,8 +4,7 @@
   <ul class="nonelist nonespace">
     <?php foreach ($tags as $tag): ?>
       <li>
-        <a class="withadd" title="Search for this tags" href="<?php echo url_for('@question_list_tags?tags='.$tag['name']); ?>"><?php echo $tag['name']; ?></a>
-        <a class="addtags" title="Add tags into search" href="<?php echo Tagged::addTagsToUrl($sf_params, $tag['name']);?>">+</a>
+        <a class="withadd" title="Search for this tags" href="<?php echo url_for('@question_list_tags?tags='.$tag['name']); ?>"><?php echo $tag['name']; ?></a><a class="addtags" title="Add tags into search" href="<?php echo Tagged::addTagsToUrl($sf_params, $tag['name']);?>">+</a>
         X <?php echo $tag['COUNT']; ?></li>
     <?php endforeach; ?>
   </ul>
@@ -19,8 +18,7 @@
     <?php foreach ($tags as $tag): ?>
       <li>
       <span class="etichetta">
-        <a class="withadd" title="Search for this" href="<?php echo url_for('@question_list_tags?tags='.$tag['name']); ?>"><?php echo $tag['name']; ?></a>
-        <a class="addtags" title="Add tags into search" href="<?php echo Tagged::addTagsToUrl($sf_params, $tag['name']);?>">+</a>
+        <a class="withadd" title="Search for this" href="<?php echo url_for('@question_list_tags?tags='.$tag['name']); ?>"><?php echo $tag['name']; ?></a><a class="addtags" title="Add tags into search" href="<?php echo Tagged::addTagsToUrl($sf_params, $tag['name']);?>">+</a>
         </span>X <?php echo $tag['COUNT']; ?></li>
     <?php endforeach; ?>
   </ul>
