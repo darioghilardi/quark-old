28cd7109f19fe38e66ee85ec489a165af9633fbc
Dario Ghilardi <darioghilardi@gmail.com>
2011-03-14 21:55:57 +0100

    Fixed tags display into homepage. They seems a bit big.

--- apps/frontend/modules/question/templates/_question_tags.php
+++ apps/frontend/modules/question/templates/_question_tags.php
@@ -1,4 +1,3 @@
 <?php foreach($tags as $tag): ?>
-  <a href="<?php echo url_for('@question_list_tags?tags='.$tag->getTag()); ?>"><?php print $tag->getTag(); ?></a>
-  <a href="<?php echo Tagged::addTagsToUrl($sf_params, $tag->getTag());?>">+</a>
+  <a class="withadd" title="Search for this tags" href="<?php echo url_for('@question_list_tags?tags='.$tag->getTag()); ?>"><?php echo $tag->getTag(); ?></a><a class="addtags" title="Add tags into search" href="<?php echo Tagged::addTagsToUrl($sf_params, $tag->getTag());?>">+</a>
 <?php endforeach; ?>
